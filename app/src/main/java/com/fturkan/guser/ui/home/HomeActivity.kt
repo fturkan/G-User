8157f0b46d665519621e5f717b392dd9be791041
Furkan Turkan <furkan.turkan@hepsijet.com>
2022-07-25 13:01:07 +0300

    Added bottom navigation bar

--- app/src/main/java/com/fturkan/guser/ui/home/HomeActivity.kt
+++ app/src/main/java/com/fturkan/guser/ui/home/HomeActivity.kt
@@ -2,11 +2,37 @@ package com.fturkan.guser.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.fturkan.guser.FavoritesFragment
+import com.fturkan.guser.HomeFragment
+import com.fturkan.guser.ProfileFragment
 import com.fturkan.guser.R
+import com.fturkan.guser.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(HomeFragment())
+
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.bottom_home_item -> replaceFragment(HomeFragment())
+                R.id.bottom_favorite_item -> replaceFragment(FavoritesFragment())
+                R.id.bottom_profile_item -> replaceFragment(ProfileFragment())
+            }
+            true
+        }
+
+    }
+
+    private fun replaceFragment(fragment : Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.homeActivity_fl, fragment)
+        transaction.commit()
     }
 }
--- app/src/main/java/com/fturkan/guser/ui/main/MainActivity.kt
+++ app/src/main/java/com/fturkan/guser/ui/main/MainActivity.kt
@@ -3,10 +3,16 @@ package com.fturkan.guser.ui.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.fturkan.guser.R
+import com.fturkan.guser.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
