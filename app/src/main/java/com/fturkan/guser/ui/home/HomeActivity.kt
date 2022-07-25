af5d22d2994257a34ebd2eb259a222039d176a2e
Furkan Turkan <furkan.turkan@hepsijet.com>
2022-07-25 11:19:08 +0300

    G-User Initial commit

---
+++ app/src/main/java/com/fturkan/guser/ui/home/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.fturkan.guser.ui.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.fturkan.guser.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
